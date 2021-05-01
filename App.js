1973da5d75832d8327ec03f315b3aa582185cee2
Balamurugan V <svbala99@gmail.com>
2021-05-01 21:31:56 +0530

    Modify App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const App = () => (
+  <View
+    style={{
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+      backgroundColor: 'white',
+    }}>
+    <Text>Hello</Text>
+  </View>
+);
+
+export default App;
